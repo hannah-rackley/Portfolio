2a2b55b4e96fb3059133fa4aaee50f29f5134da5
Hannah <hrglasser94@gmail.com>
2018-09-05 21:23:18 -0400

    Allow for expanding of cards

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ let fronts = document.querySelectorAll('.front');
 
 console.log(cards);
 
-cards.forEach(function(card) {
+let toggleRemainingCardPositions = (currentCard, card) => {
+    if (currentCard !== card) {
+        currentCard.parentElement.classList.toggle('none')
+    }
+}
+
+cards.forEach((card) => {
     card.addEventListener('click', function(event) {
-        // console.log(event.currentTarget.parentElement.parentElement);
-        card.classList.toggle('flip');
         card.parentElement.classList.toggle('expand');
-        // card.remove()
+        card.classList.toggle('flip');
+        card.parentElement.classList.toggle('flex');
+        cards.forEach(currentCard => {
+            toggleRemainingCardPositions(currentCard, card);
+        })
     })
 })
