e02640c4847bac263b83d951fd909bd588d48972
Hannah <hrglasser94@gmail.com>
2018-09-11 11:15:26 -0400

    Removed expand class and associated javascript. Adjusted header so that it is usable anywhere on the page.

--- main.js
+++ main.js
@@ -2,25 +2,25 @@ let cards = document.querySelectorAll('.card');
 let navOptions = document.querySelectorAll('.option');
 let contact = document.querySelector('.contact');
 
-let toggleRemainingCardPositions = (currentCard, card) => {
-    if (currentCard !== card) {
-        if (currentCard.classList.contains('expand')) {
-            currentCard.classList.remove('expand')
-            currentCard.scrollTop = 0;
-        }
-    }
-}
+// let toggleRemainingCardPositions = (currentCard, card) => {
+//     if (currentCard !== card) {
+//         if (currentCard.classList.contains('expand')) {
+//             currentCard.classList.remove('expand')
+//             currentCard.scrollTop = 0;
+//         }
+//     }
+// }
 
-cards.forEach((card) => {
-    card.addEventListener('click', function(event) {
-        contact.classList.remove('expand');
-        card.scrollTop = 0;
-        card.classList.toggle('expand');
-        cards.forEach(currentCard => {
-            toggleRemainingCardPositions(currentCard, card);
-        })
-    })
-})
+// cards.forEach((card) => {
+//     card.addEventListener('click', function(event) {
+//         contact.classList.remove('expand');
+//         card.scrollTop = 0;
+//         card.classList.toggle('expand');
+//         cards.forEach(currentCard => {
+//             toggleRemainingCardPositions(currentCard, card);
+//         })
+//     })
+// })
 
 //Option router - expand when you click on the navigation option in the header
 let options = {
@@ -31,19 +31,7 @@ let options = {
 
 navOptions.forEach((option) => {
     option.addEventListener('click', (event) => {
-        if (option.textContent !== 'CONTACT') {
-            contact.classList.remove('expand');
-            let card = document.querySelector(`.${options[option.textContent]}`);
-            card.scrollTop = 0;
-            card.classList.toggle('expand');
-            cards.forEach(currentCard => {
-                toggleRemainingCardPositions(currentCard, card);
-            })
-        } else {
-            contact.classList.toggle('expand');
-            cards.forEach((card) => {
-                card.classList.remove('expand');
-            })
-        }
+        let card = document.querySelector(`.${options[option.textContent]}`);
+        card.scrollTop = 0;
     })
 })
